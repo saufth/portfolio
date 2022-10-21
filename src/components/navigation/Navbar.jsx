598c3f235427aaba3ea9231af94096f72b287e23
Saufth <saufth@gmail.com>
2022-10-20 19:55:49 -0500

    ButtonBurger import removed from Navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,7 +1,6 @@
 // Components
 import Anchor from '../../core/components/navigation/Anchor'
 import Avatar from '../../core/components/data-display/Avatar'
-import BurgerButton from '../../core/components/input/BurgerButton'
 import Container from '../../core/components/layout/Container'
 // Styles
 import styles from '../../styles/navigation/Navbar.module.css'
